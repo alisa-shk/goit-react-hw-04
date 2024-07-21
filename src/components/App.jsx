294b0432015d54121df18d16048b673e8001cf0a
alisa-shk <demaliska@gmail.com>
2024-07-21 09:33:08 +0300

    first

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+import SearchBar from "../components/SearchBar/SearchBar";
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  )
+}
+
+
+export default App;
